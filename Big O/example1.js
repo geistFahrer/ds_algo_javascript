00abd7505c89bcd3ccc1e490ebac7b5b86426205
repl.it user <replituser@example.com>
2020-09-04 11:05:25 +0000

    Test

--- Big O/example1.js
+++ Big O/example1.js
@@ -3,7 +3,7 @@ const nemo = ['Nemo'];
 function findNemo(array){
   for(let i = 0; i < array.length;i++){
     if(array[i] === 'Nemo'){
-      console.log('Found Nemo');
+      console.log('Found Nemo!');
     }
   }
 }
